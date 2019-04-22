23287e910106c59f1249d521d6facc4b9240aa77
David Lawrence <dalaw85@gmail.com>
2019-04-22 10:58:11 -0400

    survey and homepage & server

---
+++ app/data/friends.js
@@ -0,0 +1,25 @@
+var friends =[
+    {
+        name: "test",
+        image: "test",
+        score:[
+            1,
+            2,
+            3,
+            3,
+            2,
+            4,
+            3,
+            2,
+            3,
+            5
+        ]
+
+    }
+
+];
+
+
+
+
+module.exports = friends;
--- app/friends.js
+++
@@ -1,6 +0,0 @@
-var freinds =[];
-
-
-
-
-module.exports = friends;
