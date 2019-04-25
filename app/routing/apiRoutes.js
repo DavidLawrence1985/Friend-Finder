b7f438056abcc8ab89365af722210a547d8c1106
David Lawrence <dalaw85@gmail.com>
2019-04-25 12:23:54 -0400

    basic funtioning app

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,14 +9,56 @@ var friends = require("../data/friends")
 module.exports = function(app){
 
     app.get("/api/friends", function(req, res){
-        res.json(friends)
+
+
+      
+        res.json(friends);
+        
     })
     
     
     
     app.post("/api/friends", function(req,res){
               friends.push(req.body);
-              res.json(friends)
+
+              for(var i = 0; i < friends.length; i++){
+
+                var newest = friends[friends.length - 1];
+        
+        
+                var x = newest.score.map(function(item, index) {
+             
+                    return Math.abs(item - friends[i].score[index]);//difference between arrays 
+                })
+                //   console.log("difference between arrays:" + x);
+                function getSum(total, num) {
+                    return total + num;
+                }
+                var y = x.reduce(getSum);//total of diffrence of arrays 
+                //   console.log("total difference:" + y);
+                
+                friends[i].value = parseInt(y);
+                console.log(friends[i].value);
+                }
+                
+                friends.pop();
+        
+                var min = Math.min.apply(null, friends.map(function(item) {
+                    return item.value;
+                    
+                  }));
+                
+                
+               best = friends.find(o => o.value === min);//o => o.value === min
+
+               friends.push(newest);
+        
+                // console.log(best);
+                
+                console.log("name: " + best.name + "\r\nimage: " + best.image );
+
+
+              res.json(best)
               
           
     })
@@ -24,4 +66,3 @@ module.exports = function(app){
 
 }
     
- 
